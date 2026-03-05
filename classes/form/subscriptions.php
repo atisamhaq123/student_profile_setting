5f9d346d210b6fc2b291694dda354d65f624e91f
Atisam <atisam82@gmail.com>
2026-03-05 20:07:07 +0500

    fix

---
+++ classes/form/subscriptions.php
@@ -0,0 +1,19 @@
+<?php
+namespace local_lat_studentsettings\form;
+
+defined('MOODLE_INTERNAL') || die();
+require_once($CFG->libdir.'/formslib.php');
+
+use moodleform;
+
+class subscriptions extends moodleform {
+    public function definition() {
+        global $OUTPUT, $CFG;
+        $mform = $this->_form;       
+
+        // SPECIAL Dropdowns
+        $mform->addElement('html', $OUTPUT->render_from_template('local_lat_studentsettings/profile/pages/subscriptions', []));
+
+
+    }
+}
